143a04007f1e285f456c965d8268bfd7ab8665e6
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-08-20 15:47:55 -0500

    code for first assembler program

--- 00_Getting_Started/sourceCode/add.s
+++ 00_Getting_Started/sourceCode/add.s
@@ -5,23 +5,26 @@ File: add.s
 Programmer: J. Evan Noynaert
 Begun:  August, 2025
 
+Assemble with GNU "as" assembler under Linux
+
 *******************************************************************************************/
 
 .globl _start
 
 .data
-   num1: .quad 6
-   num2: .quad 5
-   sum:  .quad 0
+  num1: .quad 15
+  num2: .quad 6
+  sum:  .quad 0
 
 .text
 _start:
-    # Add num1 plus num2 and store the contents in sum.   sum = num1 + num2
-    movq num1, %rax
-    addq num2, %rax
-    movq %rax, sum
 
-    # exit the program     System.exit(sum)   sys.exit(sum)
-    movq $60, %rax
-    movq sum, %rdi
-    syscall
+  # add two quad integers and store the result in sum  sum = num1 + num2
+  movq num1, %rax   # move the value stored in num1 into the rax register
+  addq num2, %rax   # add the value stored in num2 to the value alread in the rax register.
+  movq %rax, sum    # moves the contents of the rax register into sum
+
+  # use syscall to exit and return sum as an exit code.  System.exit(sum)  sys.exit(sum)
+  movq $60, %rax
+  movq sum, %rdi
+  syscall
