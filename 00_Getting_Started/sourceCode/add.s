f6577b92141eb1e31d55de6614429f6f916eeda4
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-08-19 20:50:09 -0500

    Add the add.s sourcecode

--- 00_Getting_Started/sourceCode/add.s
+++ 00_Getting_Started/sourceCode/add.s
@@ -1,19 +1,27 @@
-#This program adds two integers and returns the sum to the operating system.
+/******************************************************************************************
+This program adds two integers and returns the sum to the operating system as the exit code.
+
+File: add.s
+Programmer: J. Evan Noynaert
+Begun:  August, 2025
+
+*******************************************************************************************/
 
 .globl _start
 
 .data
-  num1: .quad 10
-  num2: .quad 15
-  sum:  .quad 0
+   num1: .quad 6
+   num2: .quad 5
+   sum:  .quad 0
 
 .text
 _start:
-  movq num1, %rax # Moves data from num1 into the rax register
-  addq num2, %rax # Adds data from num2 to the rax register
-  movq %rax, sum  # Moves the contents of rax to sum
+    # Add num1 plus num2 and store the contents in sum.   sum = num1 + num2
+    movq num1, %rax
+    addq num2, %rax
+    movq %rax, sum
 
-  # exit the program
-  mov $60,  %rax  # instruction to exit for use by syscall
-  movq sum, %rdi  # The value syscall will return to the operating system
-  syscall
+    # exit the program     System.exit(sum)   sys.exit(sum)
+    movq $60, %rax
+    movq sum, %rdi
+    syscall
