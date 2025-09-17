e70b0921c31a2848e63f08257aff45897c8e714c
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-09-17 05:38:23 -0500

    Finish unit 03 lecture notes

--- 03_IntegerArithmetic/sourcecode/sub.s
+++ 03_IntegerArithmetic/sourcecode/sub.s
@@ -6,15 +6,16 @@
 
 .globl _start
 .data
-  num1: .quad 14
-  num2: .quad  6
+  num1: .quad 200
+  num2: .quad  23
   sum:  .quad  0
 
 .text
 _start:
     # register-to-register add
     movq num1, %rax   #load the rax with the first number
-
+    subq num2, %rax   #subtract num2
+    movq %rax, sum
 
 _exit:
     #Exit the program, returning the sum
