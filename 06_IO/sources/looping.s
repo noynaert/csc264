c2d309efe6caa2b4dfa6fd5173a9073fc53fa9df
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-10-18 12:13:04 -0500

    Add example about looping and ranges

--- 06_IO/sources/looping.s
+++ 06_IO/sources/looping.s
@@ -1,6 +1,7 @@
- /*
+/* 
    Illustrates looping through a buffer with counting
-   Also, only processes a range.  It 
+   Also, only processes a range.  In its final form, the loop 
+   only prints the capital letters.
 
    file: looping.s
 
@@ -15,11 +16,44 @@
 
 .globl main
 .data
-      buffer: .ascii "(How), Now, \n[BrOwN] {CoW}!~@.123"
-  
+      buffer:  .ascii "(How), Now, \n[BrOwN] {CoW}ABZ!~@.123\n"
+      
+      #buffer: .ascii ""
+      bufferN: .quad .-buffer
 
 .text
 main:
+
+     /*
+         The loop will count down from the bufferN to zero
+     */
+
+    #prep for the loop
+    movq bufferN, %r15   #start at the length of the loop
+    movq $buffer, %rsi   #load the address of the buffer into rsi
+    addq $0, %r15        # Set the zero flag if r15 is zero
+
+    _printloop:
+    jz _endPrintLoop
+
+    cmpb $'A', (%rsi)  #compare 'A' to the address pointed to by %rsi
+    jb   _skipPrintingCharacter
+    cmpb $'Z', (%rsi)  #compare 'Z' to the address pointed to by %rsi
+    ja   _skipPrintingCharacter
+    #print the current character pointed to by %rsi
+    movq $1, %rax   # syscall for writing
+    movq $1, %rdi   # 1 is the file descriptor for console
+    #movq $buffer, %rsi   # not needed because the RSI is already loaded
+    movq $1, %rdx    # print just 1 character
+    syscall
+    _skipPrintingCharacter:
+    #prep for the next pass through the loop
+    inc %rsi
+    dec %r15
+    jmp _printloop
+
+    _endPrintLoop:
+
 
 _exit:
   movq $60, %rax
