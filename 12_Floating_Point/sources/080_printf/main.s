091e52e3f20a99bdf670ff4dfd26a3d1708b6e18
J. Evan Noynaert <noynaert@missouriwestern.edu>
2025-11-24 01:08:10 -0600

    fix program for 12_080

--- 12_Floating_Point/sources/080_printf/main.s
+++ 12_Floating_Point/sources/080_printf/main.s
@@ -1,17 +1,20 @@
 /* 
 file: main.s
 
-This program illustrates multiplying 2 floating point numbers.
+This program illustrates multiplying 2 floating point numbers and printing the results
+The program calculates the circumference of a circle.
 
 The program uses SSE technology.
+
+compile and link with gcc on Linux
 
 */
 .globl main
 .data
-   radius:   .double 10.0   # first operand
+   radius:   .double 100.0   # first operand
    pi:       .double 3.141592653589793  # second operand
    circumference: .double 0.0  # the product
-   format: .asciz "Radius: %lf, Pi: %lf, Circumference: %lf\n"
+   format: .asciz "Radius: %f, Pi: %lf, Circumference: %e\n"
 
    .text
    main:
@@ -20,17 +23,20 @@ The program uses SSE technology.
       pushq %rbp
       movq  %rsp, %rbp
 
-        # load the radius and pi into xmm registers
-      movsd radius(%rip), %xmm0  # load radius into xmm0
-      movsd pi(%rip), %xmm1      # load pi into xmm1
 
-      movsd %xmm0, %xmm2         # copy radius to xmm2 for multiplication
-      mulsd %xmm1, %xmm2         # xmm2 = radius * pi    
-      
-      #set up for printf
-      movq $format, %rdi         # first argument is format string
-      movq $3, %rax              # There are three floating point arguments for printf
+      # load the radius and pi into xmm registers
+      movsd radius, %xmm0
+      movsd pi, %xmm1
+
+      # set up and do the multiplication
+      movsd %xmm0, %xmm2 #copy the radius into xmm2 to set up multiplication
+      mulsd %xmm1, %xmm2 #multiply PI * radius, store results in xmm2
+
+      #finish setting up printf
+      movq $format, %rdi    #load the format string
+      movl $3, %eax         #3 floating point parameters
       call printf
+
 
       popq %rbp #pop the stack 
 _exit:
